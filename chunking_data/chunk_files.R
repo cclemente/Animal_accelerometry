37e95eff9ed916b8e9d8010ecec0ee8cb98a86eb
Chris Clemente <cclement@usc.edu.au>
2021-03-12 11:14:11 +1000

    Update chunk_files.R

--- chunking_data/chunk_files.R
+++ chunking_data/chunk_files.R
@@ -11,15 +11,23 @@ filenames <- (Sys.glob("*.csv"))
 ii=1
 filename[ii]
 
+#used for formating the interger in the for loop
+frmt <- "d"
+
 data<-read.csv(filenames[ii], header=F, colClasses="numeric" )
 
 # Applying split() function
 test<-split(data, ceiling(seq_along(data$V1) / 100000))
 
+#gets the name of the big accel file
 names1<-strsplit(filenames[ii], '.csv')
 
+
+#modified so the chunked files are now numbered with an interger padded with zeros e.g. 001, 002
+#this way the files are in the correct order when read back into R in the next step. 
 for (jj in 1:length(test)){
-write.csv(test[[jj]],paste0(names1,'_',as.character(jj),'.csv'))
+ij <- formatC(jj, width = 3, format = frmt, flag = "0")
+write.csv(test[[jj]],paste0(names1,'_',ij,'.csv'))
 }
 
 
